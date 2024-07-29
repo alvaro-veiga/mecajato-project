b290b2a153445dcc8abe7cbf2f95a6430762b7fe
Alvaro Veiga <alvarojoao17@gmail.com>
2024-07-29 20:23:14 -0300

    continuos improvement

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255)
+    email = models.EmailField()
+    cpf = models.CharField(max_length=11)
+
+    def __str__(self) -> str:
+        return self.nome
+
+class Carro(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    carro = models.CharField(max_length=255)
+    placa = models.CharField(max_length=8)
+    ano = models.IntegerField()
+    lavagens = models.IntegerField(default=0)
+    consertos = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.carro
