467cc3bc702b40ae69bb759174b9e5777044a8f8
Alvaro Veiga <alvarojoao17@gmail.com>
2024-10-26 13:54:30 -0300

    feat: add services

--- clientes/models.py
+++ clientes/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 class Cliente(models.Model):
-    nome = models.CharField(max_length=255)
-    sobrenome = models.CharField(max_length=255)
-    email = models.EmailField()
-    cpf = models.CharField(max_length=11)
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    cpf = models.CharField(max_length=12)
 
     def __str__(self) -> str:
         return self.nome
 
 class Carro(models.Model):
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    carro = models.CharField(max_length=255)
+    carro = models.CharField(max_length=50)
     placa = models.CharField(max_length=8)
     ano = models.IntegerField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     lavagens = models.IntegerField(default=0)
     consertos = models.IntegerField(default=0)
 
